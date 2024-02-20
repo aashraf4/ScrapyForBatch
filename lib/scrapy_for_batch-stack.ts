9dd5e3d361cbeb9416215bfed15b4a39bd8051ac
EC2 Default User <ec2-user@ip-172-31-13-15.eu-central-1.compute.internal>
2024-02-20 01:26:26 +0000

    second commit

--- lib/scrapy_for_batch-stack.ts
+++ lib/scrapy_for_batch-stack.ts
@@ -12,6 +12,141 @@ export class ScrapyForBatchStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const repo_ = new ecr.Repository(this, 'exprepoID', {
+      repositoryName: "scrapyforbatchrepo",
+    });
+    
+    const image = new ecr_assets.DockerImageAsset(this, 'MatasImage', {
+        directory: "./image-matas", // Path to Dockerfile
+    });
+    
+    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage', {
+      src: new ecrdeploy.DockerImageName(image.imageUri),
+      dest: new ecrdeploy.DockerImageName(`${repo_.repositoryUri}:matasscr`),
+    });
+        
+    const image2 = new ecr_assets.DockerImageAsset(this, 'NotinoDEImage', {
+        directory: "./image-notinode", // Path to Dockerfile
+    });
+    
+    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage2', {
+      src: new ecrdeploy.DockerImageName(image2.imageUri),
+      dest: new ecrdeploy.DockerImageName(`${repo_.repositoryUri}:notdescr`),
+    });
+  
+    const image3 = new ecr_assets.DockerImageAsset(this, 'SuperdrugImage', {
+        directory: "./image-superdrug", // Path to Dockerfile
+    });
+    
+    new ecrdeploy.ECRDeployment(this, 'DeployDockerImage3', {
+      src: new ecrdeploy.DockerImageName(image3.imageUri),
+      dest: new ecrdeploy.DockerImageName(`${repo_.repositoryUri}:superdrugscr`),
+    }); 
+    
+    const computeEnvironment = new batch.CfnComputeEnvironment(this, 'ScrapyComputeEnv', {
+      type: 'MANAGED', // Specify the type of compute environment (MANAGED or UNMANAGED)
+      computeResources: {
+        type: 'FARGATE', // Specify the type of compute resources (EC2 or FARGATE)
+        maxvCpus: 1,
+        subnets: ['subnet-0f05ebcd82dc83a33', 'subnet-0fea57fdbdc09d846', 'subnet-00f3dd1c7627d0db3'], // Specify the subnets for the compute environment
+        securityGroupIds: ['sg-0e033cd1b317e3913'], // Specify the security groups for the compute environment
+      },
+    });
+
+    const jobDefinition = new batch.CfnJobDefinition(this, 'ScrapyForBatchDef1', {
+      jobDefinitionName: 'MatasJobDef',
+      type: 'container', // Specify the type of job definition (container or multinode)
+      containerProperties: {
+        image: `${repo_.repositoryUri.toString()}:matasscr`,
+        resourceRequirements: [
+          {
+            type: 'MEMORY', // Specify memory requirement
+            value: '512', // Memory in MiB
+          },
+          {
+            type: 'VCPU', // Specify vCPU requirement
+            value: '.25', // Number of vCPUs
+          }
+        ],
+        networkConfiguration: {
+          assignPublicIp: 'ENABLED',
+        },
+        command: ['python3', 'matas.py'], // Specify the command to run in the container
+        executionRoleArn: 'arn:aws:iam::608792983808:role/simple-ecs-role', // Specify the execution role ARN
+      },
+      platformCapabilities: ['FARGATE'], // Specify Fargate platform capability
+    });
+
+
+    const jobDefinition2 = new batch.CfnJobDefinition(this, 'ScrapyForBatchDef2', {
+      jobDefinitionName: 'NotinoDEJobDef',
+      type: 'container', // Specify the type of job definition (container or multinode)
+      containerProperties: {
+        image: `${repo_.repositoryUri.toString()}:notdescr`,
+        resourceRequirements: [
+          {
+            type: 'MEMORY', // Specify memory requirement
+            value: '512', // Memory in MiB
+          },
+          {
+            type: 'VCPU', // Specify vCPU requirement
+            value: '.25', // Number of vCPUs
+          }
+        ],
+        networkConfiguration: {
+          assignPublicIp: 'ENABLED',
+        },
+        command: ['python3', 'notinoDE.py'], // Specify the command to run in the container
+        executionRoleArn: 'arn:aws:iam::608792983808:role/simple-ecs-role', // Specify the execution role ARN
+      },
+      platformCapabilities: ['FARGATE'], // Specify Fargate platform capability
+    });
+    
+    const jobDefinition3 = new batch.CfnJobDefinition(this, 'ScrapyForBatchDef3', {
+      jobDefinitionName: 'SuperdrugJobDef',
+      type: 'container', // Specify the type of job definition (container or multinode)
+      containerProperties: {
+        image: `${repo_.repositoryUri.toString()}:superdrugscr`,
+        resourceRequirements: [
+          {
+            type: 'MEMORY', // Specify memory requirement
+            value: '512', // Memory in MiB
+          },
+          {
+            type: 'VCPU', // Specify vCPU requirement
+            value: '.25', // Number of vCPUs
+          }
+        ],
+        networkConfiguration: {
+          assignPublicIp: 'ENABLED',
+        },
+        command: ['python3', 'superdrug.py'], // Specify the command to run in the container
+        executionRoleArn: 'arn:aws:iam::608792983808:role/simple-ecs-role', // Specify the execution role ARN
+      },
+      platformCapabilities: ['FARGATE'], // Specify Fargate platform capability
+    });
+
+    // Define a job queue
+    // const jobQueue = new batch.CfnJobQueue(this, 'ScrapyJobQueue', {
+    //   priority: 1, // Specify the priority of the job queue
+    //   computeEnvironmentOrder: [
+    //     {
+    //       order: 1, // Specify the order of the compute environment
+    //       computeEnvironment: computeEnvironment.ref, // Reference to the compute environment
+    //     },
+    //   ],
+    // });
+    
+    // Define a DynamoDB table with ON_DEMAND billing mode
+    const table = new dynamodb.Table(this, 'MyDynamoDBTable', {
+      tableName: "ScrapyForBatchDDB",
+      partitionKey: {
+        name: 'job_name',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST // ON_DEMAND billing mode
+    });
+
 
   }
 }
